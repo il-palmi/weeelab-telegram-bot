df1787133865e8186706efecba235af00dad0241
Lint Action <lint-action@samuelmeuli.com>
2022-09-17 05:50:23 +0000

    Fix code style issues with Black

--- salmone_standard_messages.py
+++ salmone_standard_messages.py
@@ -1,17 +1,10 @@
 import json
 
-TLS_HANDSHAKE = {
-    "command": "TLS_START"
-}
+TLS_HANDSHAKE = {"command": "TLS_START"}
 
-TLS_READY = {
-    "command": "TLS_READY"
-}
+TLS_READY = {"command": "TLS_READY"}
 
 
 def authorization_command(user: str) -> str:
-    AUTHORIZATION = {
-        "command": "GET_AUTHORIZATION",
-        "user": user
-    }
+    AUTHORIZATION = {"command": "GET_AUTHORIZATION", "user": user}
     return json.dumps(AUTHORIZATION)
